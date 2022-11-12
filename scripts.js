bd159b38544002ff2605014a4c89491d772fbfe4
Alex Martakis <51971088+Sikatram@users.noreply.github.com>
2022-11-11 17:07:00 -0800

    add scripts javascript file

---
+++ scripts.js
@@ -0,0 +1,22 @@
+console.log("scripts.js successfully loaded")
+
+function showApplications(){
+    console.log("applications is currently being displayed")
+    document.getElementById("applications").style.display = "";
+    document.getElementById("reminders").style.display = "none";
+    document.getElementById("statistics").style.display = "none";
+}
+
+function showReminders(){
+    console.log("reminders is currently being displayed")
+    document.getElementById("applications").style.display = "none";
+    document.getElementById("reminders").style.display = "";
+    document.getElementById("statistics").style.display = "none";
+}
+
+function showStatistics(){
+    console.log("statistics is currently being displayed")
+    document.getElementById("applications").style.display = "none";
+    document.getElementById("reminders").style.display = "none";
+    document.getElementById("statistics").style.display = "";
+}
