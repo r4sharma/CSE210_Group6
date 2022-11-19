08617bafd27de278ef1961a15d0539d554b553ca
r4sharma <r4sharma@ucsd.edu>
2022-11-18 18:01:17 -0800

    Reconfigured Lint to support Google style requiring JSDocs; also excluded lib directory from checks

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-console.log("scripts.js successfully loaded")
+console.log('scripts.js successfully loaded');
 
-function showApplications(){
-    console.log("applications is currently being displayed")
-    document.getElementById("applications").style.display = "";
-    document.getElementById("reminders").style.display = "none";
-    document.getElementById("statistics").style.display = "none";
+function showApplications() {
+  console.log('applications is currently being displayed');
+  document.getElementById('applications').style.display = '';
+  document.getElementById('reminders').style.display = 'none';
+  document.getElementById('statistics').style.display = 'none';
 }
 
-function showReminders(){
-    console.log("reminders is currently being displayed")
-    document.getElementById("applications").style.display = "none";
-    document.getElementById("reminders").style.display = "";
-    document.getElementById("statistics").style.display = "none";
+function showReminders() {
+  console.log('reminders is currently being displayed');
+  document.getElementById('applications').style.display = 'none';
+  document.getElementById('reminders').style.display = '';
+  document.getElementById('statistics').style.display = 'none';
 }
 
-function showStatistics(){
-    console.log("statistics is currently being displayed")
-    document.getElementById("applications").style.display = "none";
-    document.getElementById("reminders").style.display = "none";
-    document.getElementById("statistics").style.display = "";
+function showStatistics() {
+  console.log('statistics is currently being displayed');
+  document.getElementById('applications').style.display = 'none';
+  document.getElementById('reminders').style.display = 'none';
+  document.getElementById('statistics').style.display = '';
 }
