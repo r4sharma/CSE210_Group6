bef0348ab024a7731a8b0ae1173f9164a44498cd
sidbagdi <sbagdi@ucsd.edu>
2022-11-19 19:11:26 -0800

    Added Webpack config, updated cicd to run build

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// Template Source: https://webpack.js.org/concepts/configuration/
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+};
