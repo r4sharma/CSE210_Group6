7a5512fdd9090bf626e77f9ae1af0eaa59fc20d2
sidbagdi <sbagdi@ucsd.edu>
2022-11-19 21:06:32 -0800

    Added jsdocs, fixed lint issues, updated lint config

--- subtraction.js
+++ subtraction.js
@@ -1,3 +1,9 @@
+/**
+ * Sample function for testing CICD pipeline
+ * @param {int} a First Number
+ * @param {int} b Second Number
+ * @return {int} Difference
+ */
 function subtraction(a, b) {
   return (a - b);
 }
--- subtraction.test.js
+++ subtraction.test.js
@@ -1,3 +1,6 @@
+/**
+ * Sample test for checking CICD pipeline
+ */
 const subtraction = require('./subtraction');
 test('subtracts 5 - 2 to equal 3', () => {
   expect(subtraction(5, 2)).toBe(3);
