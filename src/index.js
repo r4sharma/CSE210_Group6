456685a22ecf89b61742d3bc389f096d8968cebc
YinchaoHe <yinchaohe@Yinchaos-MacBook-Pro.local>
2022-11-29 12:38:30 -0800

    restructure index.js and its tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import Database from './database/database';
+const Database = require('./database/database');
 
 /**
 @param {LoadDatabase} loadDB Run on page load/refresh
 @listens LoadDatabase
-     */
-document.addEventListener('DOMContentLoaded', (loadDB) => {
+*/
+
+// getting the element containing all job cards
+const appCardContainer = document.querySelector('#app-card-container');
+let database = null;
+
+const initializeDB = (loadDB) => {
   const name = 'Applications';
   const version = 1;
   const fields = 'jobID,'+
@@ -14,186 +19,191 @@ document.addEventListener('DOMContentLoaded', (loadDB) => {
   'doa,'+
   'applicationStatus,'+
   'description';
-  const database = new Database(name, version);
+  database = new Database(name, version);
   database.initialize(fields).then(()=>showAppCards());
   console.log(database);
-  // save button to save application in database
-  document.getElementById('save').onclick = addApplication;
-  // show form to call function to display form
-  document.getElementById('showForm').onclick = showApplicationForm;
-  // getting the element containing all job cards
-  const appCardContainer = document.querySelector('#app-card-container');
-  /**
-    Add Application Function
-    @param {event} event
-   */
-  function addApplication(event) {
-    const jobID = document.getElementById('jobid').value;
-    const companyName = document.getElementById('cname').value;
-    const jobType =
-    findRadioSelectedValue(document.getElementsByName('jobtype'));
-    const jobRole = document.getElementById('jobrole').value;
-    const doa = document.getElementById('doa').value;
-    const applicationStatus = document.getElementById('status').value;
-    const description = document.getElementById('desc').value;
-    // craeting record in db
-    const application = {jobID: jobID,
-      companyName: companyName,
-      jobType: jobType,
-      jobRole: jobRole,
-      doa: doa,
-      applicationStatus: applicationStatus,
-      description: description};
-    database.save(application)
-        .then((transaction) => {
-          document.getElementById('showForm').reset();
-          transaction.oncomplete = () => {
-            showAppCards();
-            console.log('added');
-          };
-        })
-        .catch((error) => console.log('error', error));
-  }
+};
 
-  /**
-    * Function to fetch and create all cards from db
-    */
-  function showAppCards() {
-    // Leave the div for card container empty
-    while (appCardContainer.firstChild) {
-      appCardContainer.removeChild(appCardContainer.firstChild);
-    }
-    database.getAllRecords().then(
-        (data) => {
-          createAppCards(data);
-        },
-    ).catch(() => console.log('error in fetching all records')); ;
-  }
+document.addEventListener('DOMContentLoaded', initializeDB);
 
-  /**
-   * Create app cards for all records in db
-   * @param {Array} data
-   */
-  function createAppCards(data) {
-    if (data!==null && data.length>0) {
-      data.forEach(createJobCard);
-    } else {
-      createEmptyAppCard();
-    }
-  }
-  /**
-   * Function to create Empty card in case of no applications
-   * @param {Object} value
-   */
-  function createJobCard(value) {
-    const {key,
-      jobID,
-      companyName,
-      jobType,
-      jobRole,
-      doa,
-      description,
-      applicationStatus} = value;
-    console.log(value);
-    const card = document.createElement('article');
-    card.setAttribute('id', key);
-    card.setAttribute('class', 'job-card');
-    addJobCardElement('h3', 'job-id', jobID, card);
-    addJobCardElement('h3', 'company', companyName, card);
-    addJobCardElement('p', 'job-type', jobType, card);
-    addJobCardElement('p', 'job-role', jobRole, card);
-    addJobCardElement('p', 'date-applied', doa, card);
-    addJobCardElement('p', 'description', description, card);
-    addStatusElement('p', 'job-status', applicationStatus, card);
+/**
+  Add Application Function
+  @param {event} event
+*/
+const addApplication = (event) => {
+  const jobID = document.getElementById('jobid').value;
+  const companyName = document.getElementById('cname').value;
+  const jobType =
+  findRadioSelectedValue(document.getElementsByName('jobtype'));
+  const jobRole = document.getElementById('jobrole').value;
+  const doa = document.getElementById('doa').value;
+  const applicationStatus = document.getElementById('status').value;
+  const description = document.getElementById('desc').value;
+  // craeting record in db
+  const application = {jobID: jobID,
+    companyName: companyName,
+    jobType: jobType,
+    jobRole: jobRole,
+    doa: doa,
+    applicationStatus: applicationStatus,
+    description: description};
+  database.save(application)
+      .then((transaction) => {
+        document.getElementById('showForm').reset();
+        transaction.oncomplete = () => {
+          showAppCards();
+          console.log('added');
+        };
+      })
+      .catch((error) => console.log('error', error));
+};
 
-    appCardContainer.appendChild(card);
-  }
+document.getElementById('save').addEventListener('click', addApplication);
 
-  /**
-  * Appends the element to the job card for each attribute from the job object
-  * @param {String} elementTag
-  * @param {String} id
-  * @param {String} elementContent
-  * @param {HTMLElement} card
-  * */
-  function addJobCardElement(elementTag, id, elementContent, card) {
-    const element = document.createElement(elementTag);
-    element.setAttribute('id', id);
-    element.setAttribute('class', id);
-    element.appendChild(document.createTextNode(elementContent));
-    card.appendChild(element);
-  }
-  /**
-  * Appends the status to the job card for each attribute from the job object
-  * @param {String} elementTag
-  * @param {String} id
-  * @param {String} elementContent
-  * @param {HTMLElement} card
-  * */
-  function addStatusElement(elementTag, id, elementContent, card) {
-    const element = document.createElement(elementTag);
-    element.setAttribute('id', id);
-    element.setAttribute('class', id);
-    element.appendChild(document.createTextNode(elementContent));
-    setStatusBackgroundColor(element, elementContent);
 
-    card.appendChild(element);
-  }
+/**
+  Show Application Form
+  @param {event} event
+*/
+const showApplicationForm = (event) => {
+  event.preventDefault();
+  // enable the display of app form
+  console.log('Application Form Has Been Activated');
+  document.getElementById('application-form').style.display = '';
+};
 
-  /**
+// show form to call function to display form
+// eslint-disable-next-line max-len
+document.getElementById('showForm').addEventListener('click', showApplicationForm);
+
+/**
+  * Function to fetch and create all cards from db
+*/
+function showAppCards() {
+  // Leave the div for card container empty
+  while (appCardContainer.firstChild) {
+    appCardContainer.removeChild(appCardContainer.firstChild);
+  }
+  database.getAllRecords().then(
+      (data) => {
+        createAppCards(data);
+      },
+  ).catch(() => console.log('error in fetching all records')); ;
+};
+
+/**
+ * Create app cards for all records in db
+ * @param {Array} data
+ */
+function createAppCards(data) {
+  if (data!==null && data.length>0) {
+    data.forEach(createJobCard);
+  } else {
+    createEmptyAppCard();
+  }
+}
+/**
+ * Function to create Empty card in case of no applications
+ * @param {Object} value
+ */
+function createJobCard(value) {
+  const {key,
+    jobID,
+    companyName,
+    jobType,
+    jobRole,
+    doa,
+    description,
+    applicationStatus} = value;
+  console.log(value);
+  const card = document.createElement('article');
+  card.setAttribute('id', key);
+  card.setAttribute('class', 'job-card');
+  addJobCardElement('h3', 'job-id', jobID, card);
+  addJobCardElement('h3', 'company', companyName, card);
+  addJobCardElement('p', 'job-type', jobType, card);
+  addJobCardElement('p', 'job-role', jobRole, card);
+  addJobCardElement('p', 'date-applied', doa, card);
+  addJobCardElement('p', 'description', description, card);
+  addStatusElement('p', 'job-status', applicationStatus, card);
+
+  appCardContainer.appendChild(card);
+}
+
+/**
+* Appends the element to the job card for each attribute from the job object
+* @param {String} elementTag
+* @param {String} id
+* @param {String} elementContent
+* @param {HTMLElement} card
+* */
+function addJobCardElement(elementTag, id, elementContent, card) {
+  const element = document.createElement(elementTag);
+  element.setAttribute('id', id);
+  element.setAttribute('class', id);
+  element.appendChild(document.createTextNode(elementContent));
+  card.appendChild(element);
+}
+/**
+* Appends the status to the job card for each attribute from the job object
+* @param {String} elementTag
+* @param {String} id
+* @param {String} elementContent
+* @param {HTMLElement} card
+* */
+function addStatusElement(elementTag, id, elementContent, card) {
+  const element = document.createElement(elementTag);
+  element.setAttribute('id', id);
+  element.setAttribute('class', id);
+  element.appendChild(document.createTextNode(elementContent));
+  setStatusBackgroundColor(element, elementContent);
+
+  card.appendChild(element);
+}
+
+/**
  * To set bg color indicating status of application.
  * @param {String} element
  * @param {String} elementContent
  */
-  function setStatusBackgroundColor(element, elementContent) {
-    if (elementContent == 'Applied') {
-      element.setAttribute('style', 'background-color: yellow;');
-    } else if (elementContent == 'In Progress') {
-      element.setAttribute('style', 'background-color: orange;');
-    } else if (elementContent == 'Offer') {
-      element.setAttribute('style', 'background-color: green;');
-    } else if (elementContent == 'Reject') {
-      element.setAttribute('style', 'background-color: red;');
-    } else {
-      element.setAttribute('style', 'background-color: grey;');
+function setStatusBackgroundColor(element, elementContent) {
+  if (elementContent == 'Applied') {
+    element.setAttribute('style', 'background-color: yellow;');
+  } else if (elementContent == 'In Progress') {
+    element.setAttribute('style', 'background-color: orange;');
+  } else if (elementContent == 'Offer') {
+    element.setAttribute('style', 'background-color: green;');
+  } else if (elementContent == 'Reject') {
+    element.setAttribute('style', 'background-color: red;');
+  } else {
+    element.setAttribute('style', 'background-color: grey;');
+  }
+}
+
+/**
+ * Function to create Empty card in case of no applications
+ */
+function createEmptyAppCard() {
+  if (!appCardContainer.firstChild) {
+    const text = document.createElement('p');
+    text.textContent = 'No applications to show here!';
+    appCardContainer.appendChild(text);
+  }
+}
+
+/**
+ * find the value selected in Radio button
+ * @param {HTMLElement} elements
+ * @return {string}
+ */
+function findRadioSelectedValue(elements) {
+  let val = '';
+
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      val = elements[i].value;
     }
   }
+  return val;
+}
 
-  /**
-   * Function to create Empty card in case of no applications
-   */
-  function createEmptyAppCard() {
-    if (!appCardContainer.firstChild) {
-      const text = document.createElement('p');
-      text.textContent = 'No applications to show here!';
-      appCardContainer.appendChild(text);
-    }
-  }
-
-  /**
-    Show Application Form
-    @param {event} event
-   */
-  function showApplicationForm(event) {
-    event.preventDefault();
-    // enable the display of app form
-    console.log('Application Form Has Been Activated');
-    document.getElementById('application-form').style.display = '';
-  }
-
-  /**
-   * find the value selected in Radio button
-   * @param {HTMLElement} elements
-   * @return {string}
-   */
-  function findRadioSelectedValue(elements) {
-    let val = '';
-
-    for (let i = 0; i < elements.length; i++) {
-      if (elements[i].checked) {
-        val = elements[i].value;
-      }
-    }
-    return val;
-  }
-});
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,114 @@
+/**
+ * @jest-environment jsdom
+ */
 test('Sample', async function() {
-  expect(123).toBe(123);
+  document.body.innerHTML=`        
+  <button id="showForm">Show Form</button>
+  <form id="application-form" class="application-form" style="display: none;" name="Application">
+      <label for="jobid">Job ID<i class="redtext">*</i></label> &nbsp; &nbsp;
+      <input type="text" id="jobid" name="Job ID"> <br>
+      <span class="helper-text"></span> <br> <br>
+                            
+      <label for="cname">Company Name<i class="redtext">*</i></label> <br>
+      <input type="text" id="cname" name="Company Name"> <br> 
+      <span class="helper-text"></span> <br> <br>
+                            
+      <label for="jobtype">Job Type </label> &nbsp; &nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="FullTime" checked>Full-Time</input> &nbsp;&nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="PartTime">Part-Time</input> &nbsp;&nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="Internship">Internship</input> &nbsp;&nbsp; <br>
+      <span class="helper-text"></span> <br> <br>
+  
+      <label for="jobrole">Job Role </label> &nbsp; &nbsp;
+      <input type="text" id="jobrole" name="JobRole"> <br>
+      <span class="helper-text"></span> <br> <br>
+                              
+      <label for="DOA">Date of Application <i class="redtext">*</i></label> &nbsp; &nbsp;
+      <input type="date" id="doa" name="DateOfApplication" min="2020-01-01" max="2024-12-31"> <br>
+      <span class="helper-text"></span> <br> <br>
+
+      <label for="ApplicationStatus">Application Status</label> &nbsp; &nbsp;
+      <select id="status" name="ApplicationStatus">
+      <option value="applied" selected>Applied</option>
+      <option value="inProgress">In Progress</option>
+      <option value="offer">Offer</option>
+      <option value="reject">Reject</option>
+      </select> <br> 
+      <span class="helper-text"></span> <br> <br>
+
+      <label for="description">Description</label> <br>
+      <input type="text" id="desc" name="Description"> <br>
+      <span class="helper-text"></span> <br> <br>
+
+      <input id="save" type="submit" value="SAVE">
+      <input id="cancel" type="submit" value="CANCEL"> <br>
+      <span class="helper-text"></span>
+  </form>`;
+  require(`./index.js`);
+  const button = document.getElementById('showForm');
+  button.click();
+});
+
+test('Sample1', async function() {
+  document.body.innerHTML=`        
+  <form id="application-form" class="application-form" style="display: none;" name="Application">
+      <label for="jobid">Job ID<i class="redtext">*</i></label> &nbsp; &nbsp;
+      <input type="text" id="jobid" name="Job ID"> <br>
+      <span class="helper-text"></span> <br> <br>
+                            
+      <label for="cname">Company Name<i class="redtext">*</i></label> <br>
+      <input type="text" id="cname" name="Company Name"> <br> 
+      <span class="helper-text"></span> <br> <br>
+                            
+      <label for="jobtype">Job Type </label> &nbsp; &nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="FullTime" checked>Full-Time</input> &nbsp;&nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="PartTime">Part-Time</input> &nbsp;&nbsp;
+      <input type="radio" name="jobtype" id="jobtype" value="Internship">Internship</input> &nbsp;&nbsp; <br>
+      <span class="helper-text"></span> <br> <br>
+  
+      <label for="jobrole">Job Role </label> &nbsp; &nbsp;
+      <input type="text" id="jobrole" name="JobRole"> <br>
+      <span class="helper-text"></span> <br> <br>
+                              
+      <label for="DOA">Date of Application <i class="redtext">*</i></label> &nbsp; &nbsp;
+      <input type="date" id="doa" name="DateOfApplication" min="2020-01-01" max="2024-12-31"> <br>
+      <span class="helper-text"></span> <br> <br>
+
+      <label for="ApplicationStatus">Application Status</label> &nbsp; &nbsp;
+      <select id="status" name="ApplicationStatus">
+      <option value="applied" selected>Applied</option>
+      <option value="inProgress">In Progress</option>
+      <option value="offer">Offer</option>
+      <option value="reject">Reject</option>
+      </select> <br> 
+      <span class="helper-text"></span> <br> <br>
+
+      <label for="description">Description</label> <br>
+      <input type="text" id="desc" name="Description"> <br>
+      <span class="helper-text"></span> <br> <br>
+      <button id="save">Save</button>
+      <input id="cancel" type="submit" value="CANCEL"> <br>
+      <span class="helper-text"></span>
+  </form>`;
+  require(`./index.js`);
+
+  const jobIDElement = document.getElementById('jobid');
+  jobIDElement.value = `1`;
+  const companyNameElement = document.getElementById('cname');
+  companyNameElement.value = `UCSD`;
+  const jobTypeElement = document.getElementsByName('jobtype');
+  jobTypeElement.value = `Full-time`;
+  const jobRoleElement = document.getElementById('jobrole');
+  jobRoleElement.value = `SDE`;
+  const doaElement = document.getElementById('doa');
+  doaElement.value = `2020-01-01`;
+  const applicationStatusElement = document.getElementById('status');
+  applicationStatusElement.value = `offer`;
+  const descriptionElement = document.getElementById('desc');
+  descriptionElement.value = `good`;
+
+  const button = document.getElementById('save');
+  console.log(button.innerHTML);
+  button.click();
+  expect(descriptionElement.innerHTML).toBe(``);
 });
