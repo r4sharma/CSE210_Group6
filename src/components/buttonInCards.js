b59091d5a20f8c1b4c044a9bb8433e98fdcee4dc
Purva Kothari <kotharipurva@gmail.com>
2022-12-02 18:33:39 -0800

    Bug fix for delete edit functionality

--- src/components/buttonInCards.js
+++ src/components/buttonInCards.js
@@ -12,30 +12,18 @@ export function addJobCardEditButton(parent, key) {
   editButton.setAttribute('type', 'submit');
   editButton.innerHTML = 'Edit';
   editButton.setAttribute('id', 'edit-app');
-  addEditModal(editButton, key);
+  editButton.setAttribute('data-bs-toggle', 'modal');
+  editButton.setAttribute('data-bs-target', '#updateApp');
+  editButton.setAttribute('data-id', key);
+  editButton.onclick = createEditModal;
   parent.appendChild(editButton);
 }
 
 /**
-* Creates a edit modal of job card
-* @param {HTMLElement} editButton
-* @param {int} key
-*/
-function addEditModal(editButton, key) {
-  editButton.setAttribute('data-bs-toggle', 'modal');
-  editButton.setAttribute('data-bs-target', '#updateApp');
-  editButton.setAttribute('data-id', key);
-  editButton.onclick = populateEditModal;
-  const editButtonModal = document.querySelector('#editAppButton');
-  editButtonModal.setAttribute('data-id', key);
-  editButtonModal.onclick = editViaModal;
-}
-
-/**
- * Populate details of a particular record
- * @param {event} event
- */
-function populateEditModal(event) {
+   * Populate details of a particular record
+   * @param {event} event
+   */
+function createEditModal(event) {
   const key = Number(event.target.getAttribute('data-id'));
   console.log(key);
   database.getRecordByKey(key).then((value)=>{
@@ -44,26 +32,34 @@ function populateEditModal(event) {
     document.getElementById('editJobid').setAttribute('value', value.jobID);
     document.getElementById('editCname')
         .setAttribute('value', value.companyName);
-    document.getElementById('editJobtype').checked = value.jobType;
-    document.getElementById('editApplicationStatus')
-        .selected =value.applicationStatus;
+    document.getElementById(value.jobType).checked = true;
+    // eslint-disable-next-line max-len
+    for (const option of document.getElementById('editApplicationStatus').options) {
+      if (option.value === value.applicationStatus) {
+        option.selected = true;
+      }
+    }
+    // document.getElementById('editApplicationStatus')
+    //     .selected =value.applicationStatus;
     document.getElementById('editJobrole')
         .setAttribute('value', value.jobRole);
     document.getElementById('editDoa')
         .setAttribute('value', value.doa);
     document.getElementById('editDescription')
-        .setAttribute('value', value.description);
+        .innerHTML=value.description;
   });
+
+  const editSaveButtonModal = document.querySelector('#editAppButton');
+  editSaveButtonModal.setAttribute('data-id', key);
+  editSaveButtonModal.onclick = editViaModal;
 }
 
 /**
- * Adds edit functionality to edit button in modal
- * @param {event} event
- */
+   * Adds edit functionality to edit button in modal
+   * @param {event} event
+   */
 function editViaModal(event) {
   event.preventDefault();
-  const task = event.currentTarget.closest('.message');
-  console.log(task);
   const key = Number(event.target.getAttribute('data-id'));
   editApplication(key);
 }
@@ -76,14 +72,16 @@ function editApplication(key) {
   const jobID = document.getElementById('editJobid').value;
   const companyName = document.getElementById('editCname').value;
   const jobType =
-      findRadioSelectedValue(document.getElementsByName('editJobtype'));
+  findRadioSelectedValue(document.getElementsByName('editJobtype'));
   const jobRole = document.getElementById('editJobrole').value;
   const doa = document.getElementById('editDoa').value;
   const applicationStatus =
-        document.getElementById('editApplicationStatus').value;
+    document.getElementById('editApplicationStatus').value;
   const description = document.getElementById('editDescription').value;
   // craeting record in db
-  const application = {jobID: jobID,
+  const application = {
+    key: key,
+    jobID: jobID,
     companyName: companyName,
     jobType: jobType,
     jobRole: jobRole,
@@ -94,10 +92,7 @@ function editApplication(key) {
   database.update(application)
       .then((transaction) => {
         document.getElementById('edit-form').reset();
-        transaction.oncomplete = () => {
-          showAppCards();
-          console.log('updated');
-        };
+        showAppCards();
       })
       .catch((error) => console.log('error in updating the record.', error));
 }
@@ -109,35 +104,35 @@ function editApplication(key) {
 * */
 export function addJobCardDeleteButton(parent, key) {
   const deleteButton = document.createElement('button');
-  deleteButton.setAttribute('class', 'btn btn-outline-danger btn-sm');
+  deleteButton.classList.add('btn',
+      'btn-outline-danger',
+      'btn-sm');
   deleteButton.setAttribute('type', 'submit');
   deleteButton.setAttribute('id', 'delete-app');
   deleteButton.innerHTML = 'Delete';
-
-  addDelModal(deleteButton, key);
+  deleteButton.setAttribute('data-bs-toggle', 'modal');
+  deleteButton.setAttribute('data-bs-target', '#deleteApp');
+  deleteButton.setAttribute('data-id', key);
+  deleteButton.onclick = createDeleteModal;
   parent.appendChild(deleteButton);
 }
 
 /**
-* Creates a delete button of job card
-* @param {HTMLElement} deleteButton
-* @param {int} key
-* */
-function addDelModal(deleteButton, key) {
-  deleteButton.setAttribute('data-bs-toggle', 'modal');
-  deleteButton.setAttribute('data-bs-target', '#deleteApp');
+   * Function to create modal for delete
+   * @param {event} event
+   */
+function createDeleteModal(event) {
+  const key = Number(event.target.getAttribute('data-id'));
   const deleteButtonModal = document.querySelector('#deleteAppButton');
   deleteButtonModal.setAttribute('data-id', key);
   deleteButtonModal.onclick = deleteViaModal;
 }
-
 /**
  * Adds delete functionality to delete button in modal
  * @param {event} event
  */
 function deleteViaModal(event) {
   event.preventDefault();
-  console.log(event.target);
   const key = Number(event.target.getAttribute('data-id'));
   deleteApplication(key);
 }
