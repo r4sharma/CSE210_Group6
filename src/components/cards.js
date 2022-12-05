179163e2248ebf4f3f49998a9d1e754040253a91
alexmartakis@gmail.com <51971088+Sikatram@users.noreply.github.com>
2022-12-05 11:17:44 -0800

    Implement new UI theme colors

--- src/components/cards.js
+++ src/components/cards.js
@@ -146,7 +146,7 @@ function createJobCardColumnTwo(parent, applicationStatus) {
   bodyTwo.appendChild(coloredColumn);
 
   const status = document.createElement('h5');
-  status.setAttribute('class', 'text-light');
+  status.setAttribute('class', 'text');
   status.innerHTML = applicationStatus;
   coloredColumn.appendChild(status);
 }
@@ -268,18 +268,18 @@ function createDateAppliedElement(parent, doa) {
 * @return {String}
 */
 function setStatusBackgroundColor(status) {
-  if (status == 'applied') {
+  if (status == 'inProgress') {
     // eslint-disable-next-line max-len
-    return 'col-sm rounded-bottom-right-1 bg-warning d-flex align-items-center';
-  } else if (status == 'inProgress') {
+    return 'col-sm rounded-bottom-right-1 bg-warning-custom-status d-flex align-items-center';
+  } else if (status == 'applied') {
     // eslint-disable-next-line max-len
-    return 'col-sm rounded-bottom-right-1 bg-primary d-flex align-items-center';
+    return 'col-sm rounded-bottom-right-1 bg-primary-custom-status d-flex align-items-center';
   } else if (status == 'offer') {
     // eslint-disable-next-line max-len
-    return 'col-sm rounded-bottom-right-1 bg-success d-flex align-items-center';
+    return 'col-sm rounded-bottom-right-1 bg-success-custom-status d-flex align-items-center';
   } else if (status == 'reject') {
     // eslint-disable-next-line max-len
-    return 'col-sm rounded-bottom-right-1 bg-danger d-flex align-items-center';
+    return 'col-sm rounded-bottom-right-1 bg-danger-custom-status d-flex align-items-center';
   } else {
     return 'col-sm rounded-bottom-right-1 bg-muted d-flex align-items-center';
   }
