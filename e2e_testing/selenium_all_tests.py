606ec6b8b979679f02cf06eb625246fd8e93175d
Mohammad Anas Mudassir <mohdmmedanas@gmail.com>
2022-12-06 14:47:36 -0800

    Corrected the file location for running all tests. Added pycache to gitignore

--- e2e_testing/selenium_all_tests.py
+++ e2e_testing/selenium_all_tests.py
@@ -14,7 +14,7 @@ parent_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 if os.name == 'nt':
     tests_directory_path = parent_path + "\e2e_testing\\"
 elif os.name == 'posix':
-    tests_directory_path = "file:///" + parent_path + "/e2e_testing/"
+    tests_directory_path = parent_path + "/e2e_testing/"
 
 os.system("python " + tests_directory_path + "selenium_insert_test.py")
 os.system("python " + tests_directory_path + "selenium_delete_test.py")
